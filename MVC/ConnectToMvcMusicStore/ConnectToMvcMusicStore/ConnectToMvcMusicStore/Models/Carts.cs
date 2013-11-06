3fdd69a46b28e25c81cda3e66c3530150a4048a9
PGuihen <paul.guihen@gmail.com>
2013-11-06 14:22:24 +0000

    adding orderdetails

---
+++ MVC/ConnectToMvcMusicStore/ConnectToMvcMusicStore/ConnectToMvcMusicStore/Models/Carts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConnectToMvcMusicStore.Models
+{
+    public class Carts : Controller
+    {
+        //
+        // GET: /Carts/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
