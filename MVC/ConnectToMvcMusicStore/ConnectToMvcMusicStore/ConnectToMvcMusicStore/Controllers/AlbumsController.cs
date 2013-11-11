ced898f8260b2f6e900a49dc28caea94181cbb5e
PGuihen <paul.guihen@gmail.com>
2013-11-11 20:50:37 +0000

    albums commit

---
+++ MVC/ConnectToMvcMusicStore/ConnectToMvcMusicStore/ConnectToMvcMusicStore/Controllers/AlbumsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConnectToMvcMusicStore.Models;
+
+namespace ConnectToMvcMusicStore.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        private AlbumsDBContext db = new AlbumsDBContext();
+
+        //
+        // GET: /Albums/
+
+        public ActionResult Index()
+        {
+            return View(db.AlbumsDbSet.ToList());
+        }
+
+        //
+        // GET: /Albums/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Albums albums = db.AlbumsDbSet.Find(id);
+            if (albums == null)
+            {
+                return HttpNotFound();
+            }
+            return View(albums);
+        }
+
+        //
+        // GET: /Albums/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Albums/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Albums albums)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AlbumsDbSet.Add(albums);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(albums);
+        }
+
+        //
+        // GET: /Albums/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Albums albums = db.AlbumsDbSet.Find(id);
+            if (albums == null)
+            {
+                return HttpNotFound();
+            }
+            return View(albums);
+        }
+
+        //
+        // POST: /Albums/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Albums albums)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(albums).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(albums);
+        }
+
+        //
+        // GET: /Albums/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Albums albums = db.AlbumsDbSet.Find(id);
+            if (albums == null)
+            {
+                return HttpNotFound();
+            }
+            return View(albums);
+        }
+
+        //
+        // POST: /Albums/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Albums albums = db.AlbumsDbSet.Find(id);
+            db.AlbumsDbSet.Remove(albums);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC/ConnectToMvcMusicStore/ConnectToMvcMusicStore/ConnectToMvcMusicStore/Models/Albums.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConnectToMvcMusicStore.Models
+{
+    public class Albums
+    {
+         [Key]
+        public int AlbumId { get; set; }
+        public int GenreId { get; set; }
+        public int ArtistId { get; set; }
+        public string Title{ get; set; }
+        public decimal Price { get; set; }
+        public string AlbumArtUrl { get; set; }
+       
+    }
+
+    
+
+    public class AlbumsDBContext : DbContext
+    {
+       /* protected override void OnModelCreating(Artists artists)
+        {
+            artists.IncludeMetadataInDatabase = false;
+        }*/
+
+        static AlbumsDBContext()
+        {
+            Database.SetInitializer<Models.AlbumsDBContext>(null);
+        }
+
+       // Database.SetInitializer<Models.YourDbContext>(null);
+
+       // System.Data.Entity.Database.SetInitializer<MvcMusicStoreDBContext>(null); 
+
+        public DbSet<Albums> AlbumsDbSet { get; set; }
+    }
+}
+
+/*
+ 
+ 
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ConnectToMvcMusicStore.Models
+{
+    public class ArtistsModels
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }   
+
+    public class MvcMusicStoreDBContext  : DbContext
+    {
+        public DbSet<ArtistsModels> Artists { get; set; }
+    }
+}*/
+    
